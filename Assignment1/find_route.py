51091d7813ff7a2ddf0ada2c09935fb9ea97aa7e
Roger <70551403+BoiRogg@users.noreply.github.com>
2021-06-28 16:29:12 -0500

    working on informed search

--- Assignment1/find_route.py
+++ Assignment1/find_route.py
@@ -1,3 +1,4 @@
+from os import readlink
 import sys
 
 def extract(route, path):
@@ -12,12 +13,14 @@ def extract(route, path):
         return path
 
 def uninformedSearch(routes, origin, dest):
-    result = UCS({origin: [0,[origin]]}, {}, routes, origin, dest, [0, 0], [origin])
+    result = UCS({origin: [0,[origin]]}, {}, routes, origin, dest, [0, 0, 0])
 
     if(result[0][0] > -1):
-        print('Nodes expanded:' + str(result[1][0]) + '\nNodes generated:' + str(result[1][1]))
-        print('Distance:' + str(result[0][0]) + 'km')
-        print('Route:')
+        print('Nodes popped: ' + str(result[1][2])) 
+        print('Nodes expanded: ' + str(result[1][0]))
+        print('Nodes generated: ' + str(result[1][1]))
+        print('Distance: ' + str(result[0][0]) + 'km')
+        print('Route: ')
         route = result[0][1]
         route = extract(route, [])
         if(route):
@@ -28,13 +31,14 @@ def uninformedSearch(routes, origin, dest):
             print(origin, 'to', str(dest) + ',', result[0][0], 'km')
 
     else:
-        print('Nodes expanded:' + str(result[0][1][0]) + '\nNodes generated:' + str(result[0][1][1]))
+        print('Nodes popped: ' + str(result[0][1][2])) 
+        print('Nodes expanded: ' + str(result[0][1][0]))
+        print('Nodes generated: ' + str(result[0][1][1]))
         print('Distance: Infinity')
         print('Route:\nNone')
 
     return
-
-def UCS(fringe, visited, routes, origin, dest, nodes, path):
+def UCS(fringe, closedSet, routes, origin, dest, nodes):
     
     if(not fringe):
         return [[-1, nodes]]
@@ -47,26 +51,70 @@ def UCS(fringe, visited, routes, origin, dest, nodes, path):
             return [fringe[closest], nodes]
           
         currPath = fringe.pop(closest)
-        visited[closest] = currPath
+        closedSet[closest] = currPath
 
         for city in routes[closest]:
             nodes[1] = nodes[1] + 1
 
-            if(not (city in visited or city in fringe)):
+            if(not (city in closedSet or city in fringe)):
                 fringe[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
+                nodes[2] = nodes[2] + 1
 
-            elif(city in fringe or city in visited):
+            elif(city in fringe or city in closedSet):
 
-                if(city in visited and visited[city][0] > currPath[0] + routes[closest][city]):
-                    visited[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
-                    fringe[city] = visited.pop(city)
-
+                if(city in closedSet and closedSet[city][0] > currPath[0] + routes[closest][city]):
+                    closedSet[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
+                    fringe[city] = closedSet.pop(city)
+                    
                 elif(city in fringe and fringe[city][0] > currPath[0] + routes[closest][city]):
                     fringe[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
+                    
 
-    return UCS(fringe, visited, routes, origin, dest, nodes, path)
+    return UCS(fringe, closedSet, routes, origin, dest, nodes)
 
-def informedSearch():
+class Node: 
+    def __init__(self, prev, current, total, dist):
+        self.prev = prev
+        self.total = total + dist
+        self.current = current
+
+def readHFile(hFile):
+    hValues = {}
+    f = open(hFile, 'r')
+    input = f.readline()
+
+    while("END OF INPUT" not in input):
+        input = input.split()
+        distance = int(input[1])
+        hValues[input[0]] = distance
+        input = f.readline()
+    
+    return hValues
+
+def informedSearch(routes, origin, dest, huer):
+    closedSet = []
+    fringe = []
+
+    fringe.append(Node(None, origin, 0, 0))
+    nodesCreated = 0
+    nodesExp = 0
+    nodesPop = 0
+
+    def loop(nodesCreated, nodesExp, nodesPop):
+        nodesExp += 1
+        node = fringe.pop(0)
+
+        if(node.current == dest):
+            quit()
+
+        loop(nodesCreated, nodesExp, nodesPop)
+
+
+    loop(nodesCreated, nodesExp, nodesPop)
+
+
+
+
     
     return
 
@@ -96,10 +144,10 @@ else:
         print(dest, 'does not exist.')
     if(dest not in routes or origin not in routes):
         quit()
-        
 
     if(len(sys.argv) == 5):
         hFile = sys.argv[4]
-        informedSearch(routes, origin, dest, hFile)
+        informedSearch(routes, origin, dest, readHFile(hFile))
+
     else:
         uninformedSearch(routes, origin, dest)
