01ee30c6f926351dc02758c2cca2e864d909908f
Roger <70551403+BoiRogg@users.noreply.github.com>
2021-06-30 12:23:40 -0500

    cleaned up code

--- Assignment1/find_route.py
+++ Assignment1/find_route.py
@@ -1,76 +1,4 @@
-from os import readlink
 import sys
-
-# def extract(route, path):
-#     if(not route): 
-#         return path
-#     if(len(route) == 1):
-#         path.append(route[0])
-#     else:
-#         path.append(route[1])
-#         route = route[0]
-#         extract(route, path)
-#         return path
-
-# def uninformedSearch(routes, origin, dest):
-#     result = UCS({origin: [0,[origin]]}, {}, routes, origin, dest, [0, 0, 0])
-
-#     if(result[0][0] > -1):
-#         print('Nodes popped: ' + str(result[1][2])) 
-#         print('Nodes expanded: ' + str(result[1][0]))
-#         print('Nodes generated: ' + str(result[1][1]))
-#         print('Distance: ' + str(result[0][0]) + 'km')
-#         print('Route: ')
-#         route = result[0][1]
-#         route = extract(route, [])
-#         if(route):
-#             route.reverse()
-#             for i in range(len(route)-1):
-#                 print(route[i], 'to', route[i+1], routes[route[i]][route[i+1]], 'km')
-#         else:
-#             print(origin, 'to', str(dest) + ',', result[0][0], 'km')
-
-#     else:
-#         print('Nodes popped: ' + str(result[0][1][2])) 
-#         print('Nodes expanded: ' + str(result[0][1][0]))
-#         print('Nodes generated: ' + str(result[0][1][1]))
-#         print('Distance: Infinity')
-#         print('Route:\nNone')
-
-#     return
-# def UCS(fringe, closedSet, routes, origin, dest, nodes):
-    
-#     if(not fringe):
-#         return [[-1, nodes]]
-
-#     else:
-#         nodes[0] = nodes[0] + 1 
-#         closest = min(fringe, key = (lambda k:fringe[k][0]))
-
-#         if(dest == closest):
-#             return [fringe[closest], nodes]
-          
-#         currPath = fringe.pop(closest)
-#         closedSet[closest] = currPath
-
-#         for city in routes[closest]:
-#             nodes[1] = nodes[1] + 1
-
-#             if(not (city in closedSet or city in fringe)):
-#                 fringe[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
-#                 nodes[2] = nodes[2] + 1
-
-#             elif(city in fringe or city in closedSet):
-
-#                 if(city in closedSet and closedSet[city][0] > currPath[0] + routes[closest][city]):
-#                     closedSet[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
-#                     fringe[city] = closedSet.pop(city)
-                    
-#                 elif(city in fringe and fringe[city][0] > currPath[0] + routes[closest][city]):
-#                     fringe[city] = [currPath[0] + routes[closest][city], [currPath[1], city]]
-                    
-
-#     return UCS(fringe, closedSet, routes, origin, dest, nodes)
 
 class Node: 
     def __init__(self, prev, current, total, distCity):
@@ -99,6 +27,7 @@ def uninformedSearch(routes, origin, dest):
     nodesPop = 0
 
     fringe.append(Node(None, origin, 0, 0))
+    nodesGen += 1
 
     def loop(nodesGen, nodesExp, nodesPop):
         if(len(fringe) == 0 ):
@@ -224,13 +153,10 @@ def informedSearch(routes, origin, dest, huer):
 
     return
 
-format = ("Not enough arguments.\nPlease use format:\n" 
-"python find_route.py [inputfile.txt] [origin city] [destination city]\n"
-"   or\n"
-"python find_route.py [inputfile.txt] [origin city] [destination city] [heuristicfile.txt]\n")
-
 if(len(sys.argv) < 4 or len(sys.argv) > 5):
-    print(format)
+    print("Not enough arguments, please use on of the formats:")
+    print("python find_route.py [inputfile.txt] [origin city] [destination city]")
+    print("python find_route.py [inputfile.txt] [origin city] [destination city] [heuristicfile.txt]")
 else:
     file = open(sys.argv[1], "r")
     input = file.readline()
