41922d2a9f860221717ab6ceace09311a0b3b3d7
Roger <70551403+BoiRogg@users.noreply.github.com>
2021-06-30 22:24:26 -0500

    Added comments
    adjust 'total' in Node class

--- Assignment1/README.txt
+++ Assignment1/README.txt
@@ -1,12 +1,49 @@
 Rogelio Chapa  
 1000794793
-Python 3.9.5
+Python 3.9.5 (Windows 10)
+Did not test with omega
 
-How to compile code:
+How to run code:
 python find_route.py [inputfile.txt] [origin city] [destination city]
 
 When using hueristic file use:
 python find_route.py [inputfile.txt] [origin city] [destination city] [heuristicfile.txt] 
 
+* These commands must be typed from the same directory
+as the find_route.py file, input and hueristic file must also be in the
+same directory as the find_route.py file
 
 Structure:
+
+class Nodes:
+
+This is a class that will keep track of nodes(cities), it will hold
+the nodes current city, previous node(from prev city), distance to 
+current city from previous node and total distance it has traveled so 
+far from origin
+
+def readHFile(hFile):
+
+This will open a read hueristic file and return a dictonary
+of the cities along with their hueristic values
+
+def uninformedSearch(routes, origin, dest):
+
+This will permform the uninformed search. It will take in the 
+routes which is a dictionary of all the cities, each city will have a 
+smaller dictionary within it for each possible path along with the cost.
+It will also take in the origin (start city) and dest (final destination)
+
+def informedSearch(routes, origin, dest, huer):
+
+Similar to uninformed search, this will perform an informed search.
+It will also take in routes which is a dictionary of cities and all their
+possible paths and costs. Origin and dest are the start and end city. 
+The huer will be the dictionary of hueristic values for each city
+
+main area:
+
+This is where the code will start and make all the calls. It will do
+the type checking and make sure the run command is called with 
+enough arguments. It will also open the input file containing all
+the city map and create the routes dictionary.
--- Assignment1/find_route.py
+++ Assignment1/find_route.py
@@ -1,12 +1,17 @@
 import sys
 
+#This class will bout our node and will keep track of the previous node,
+#the current city, the distance to current city from prev node, and the
+#total distance traveled so far
 class Node: 
     def __init__(self, prev, current, total, distCity):
         self.prev = prev
-        self.total = total + distCity
+        self.total = total
         self.dist = distCity
         self.current = current
 
+#Takes in the hueristic file name and reads file
+#to create a dictionary of cities along with hueristic  values
 def readHFile(hFile):
     hValues = {}
     f = open(hFile, 'r')
@@ -20,6 +25,9 @@ def readHFile(hFile):
     
     return hValues
 
+#Performs uninformed search, takes in routes which is a dictionary of each city, each city
+#has a smaller dictionary of all the paths it can take and their cost. Will also take origin
+#which is the start city and dest which is the destination city.
 def uninformedSearch(routes, origin, dest):
     closedSet = []
     fringe = []
@@ -27,9 +35,11 @@ def uninformedSearch(routes, origin, dest):
     nodesExp = 0
     nodesPop = 0
 
+    #create the first nodes to go on fringe from origin city
     fringe.append(Node(None, origin, 0, 0))
     nodesGen += 1
 
+    #Check for empty fringe at start of loop, reaching this point means no route was found
     def loop(nodesGen, nodesExp, nodesPop):
         if(len(fringe) == 0 ):
             print("Nodes popped:", nodesPop)
@@ -39,25 +49,27 @@ def uninformedSearch(routes, origin, dest):
             print("Route:\nNone")
             quit()
         
+        
         node = fringe.pop()
         nodesPop += 1
 
+        #Pop fringe in to node and check if it contains the destination city, if so
+        #we have found the solution
         if(node.current == dest):
             prevCity = node.prev
             path = []
             distCity = []
-            totalDist = 0
+            totalDist = node.total
             
             while prevCity != None:
                 path.append(node.current)
                 path.append(prevCity.current)
                 distCity.append(node.dist)
-                totalDist += node.dist
                 prevCity = prevCity.prev
                 node = node.prev 
 
             print("Nodes popped:", nodesPop)
-            print("Nodes Expanded:", nodesExp)
+            print("Nodes expanded:", nodesExp)
             print("Nodes generated", nodesGen)
             print("Distance:", totalDist, "km")
             print("Route:")
@@ -67,9 +79,15 @@ def uninformedSearch(routes, origin, dest):
                 
             return
 
+        #If current city (node.current) is in closedSet, then we have already explored this cities
+        #paths, we will skip it and just run next iteration of loop
         if(node.current in closedSet):
             return loop(nodesGen, nodesExp, nodesPop)
-
+        
+        #If current city (node.current) was not in closed set this mean we have not explored this
+        #cities paths, we will now add the current city to the closed set and add all paths to the 
+        #fringe, the fringe is then sorted by total dist traveled so far
+        #the node/city with the lowest value will be at the top of the fringe
         else:
             nodesExp += 1
             closedSet.append(node.current)
@@ -86,6 +104,10 @@ def uninformedSearch(routes, origin, dest):
 
     return
 
+#Performs informed search, takes in routes which is a dictionary of each city, each city
+#has a smaller dictionary of all the paths it can take and their cost. Will also take origin
+#which is the start city and dest which is the destination city. In addition it will also take
+#huer which is a dictionary of citites along with their hueristic values
 def informedSearch(routes, origin, dest, huer):
     closedSet = []
     fringe = []
@@ -93,9 +115,11 @@ def informedSearch(routes, origin, dest, huer):
     nodesExp = 0
     nodesPop = 0
 
+    #create the first nodes to go on fringe from origin city
     fringe.append(Node(None, origin, 0, 0))
     nodesGen += 1
 
+    #Check for empty fringe at start of loop, reaching this point means no route was found
     def loop(nodesGen, nodesExp, nodesPop, huer):
         if(len(fringe) == 0 ):
             print("Nodes popped:", nodesPop)
@@ -108,22 +132,23 @@ def informedSearch(routes, origin, dest, huer):
         node = fringe.pop()
         nodesPop += 1
 
+        #Pop fringe in to node and check if it contains the destination city, if so
+        #we have found the solution
         if(node.current == dest):
             prevCity = node.prev
             path = []
             distCity = []
-            totalDist = 0
+            totalDist = node.total
             
             while prevCity != None:
                 path.append(node.current)
                 path.append(prevCity.current)
                 distCity.append(node.dist)
-                totalDist += node.dist
                 prevCity = prevCity.prev
                 node = node.prev 
 
             print("Nodes popped:", nodesPop)
-            print("Nodes Expanded:", nodesExp)
+            print("Nodes expanded:", nodesExp)
             print("Nodes generated:", nodesGen)
             print("Distance:", totalDist, "km")
             print("Route:")                
@@ -133,9 +158,15 @@ def informedSearch(routes, origin, dest, huer):
                 
             return
 
+        #If current city (node.current) is in closedSet, then we have already explored this cities
+        #paths, we will skip it and just run next iteration of loop
         if(node.current in closedSet):
             return loop(nodesGen, nodesExp, nodesPop, huer)
 
+        #If current city (node.current) was not in closed set this mean we have not explored this
+        #cities paths, we will now add the current city to the closed set and add all paths to the 
+        #fringe, the fringe is then sorted by total distance traveled so far plus the hueristic value
+        #the node/city with the lowest value will be at the top of the fringe
         else:
             nodesExp += 1
             closedSet.append(node.current)
@@ -144,7 +175,7 @@ def informedSearch(routes, origin, dest, huer):
                 nodesGen += 1
                 fringe.append(Node(node, city, routes[node.current][city] + node.total, routes[node.current][city]))
 
-            fringe.sort(key = (lambda x: x.dist + huer[x.current]), reverse = True)
+            fringe.sort(key = (lambda x: x.total + huer[x.current]), reverse = True)
 
         loop(nodesGen, nodesExp, nodesPop, huer)
 
@@ -152,10 +183,14 @@ def informedSearch(routes, origin, dest, huer):
 
     return
 
+#Type checking for the length of command arguemnts, print out correct format when incorrect amount
+#of args are entered
 if(len(sys.argv) < 4 or len(sys.argv) > 5):
     print("Not enough arguments, please use on of the formats:")
     print("python find_route.py [inputfile.txt] [origin city] [destination city]")
     print("python find_route.py [inputfile.txt] [origin city] [destination city] [heuristicfile.txt]")
+
+#If correct command format is entered we will read file of input in to routes dictionary
 else:
     file = open(sys.argv[1], "r")
     input = file.readline()
@@ -163,6 +198,8 @@ else:
     dest = sys.argv[3]
     routes = {}
     
+    #This will create a dictionary of routes for each city, each city element in the dictionary
+    #will have a smaller dictionary containing all its paths and cost
     while("END OF INPUT" not in input):
         input = input.split()
         routes.setdefault(input[0], {})[input[1]] = int(input[2])
@@ -176,9 +213,12 @@ else:
     if(dest not in routes or origin not in routes):
         quit()
 
+    #If a 5th argument was etner it will be the hueristic file so we call informed search and
+    #readHFile to create hueristic dictionary
     if(len(sys.argv) == 5):
         hFile = sys.argv[4]
         informedSearch(routes, origin, dest, readHFile(hFile))
-
+    
+    #If no 5th argument then we just perform uninformed search
     else:
         uninformedSearch(routes, origin, dest)
